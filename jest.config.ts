6e52062ab2ccf634c3aaa1ea91a47d23c8463035
Sam Pullman <sampullman@gmail.com>
2022-02-09 00:11:28 +0800

    test: add jest and basic tests #1

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: '@samatech/vue-store',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js'],
+  testMatch: ['**/lib/**/+(*.)+(spec).+(ts)', '**/test/**/+(*.)+(spec).+(ts)'],
+  resetMocks: false,
+  setupFiles: ['jest-localstorage-mock'],
+}

9d6bb172b8f038d69ba198211ed7a79b8be32f55
abemscac <abe.msc.ac@gmail.com>
2022-03-20 12:17:00 +0800

    feat: replace store with module

--- lib/module.ts
+++ lib/module.ts
@@ -1,60 +1,140 @@
-import { ComputedRef, DeepReadonly, Ref } from 'vue'
+import {
+  computed,
+  ComputedRef,
+  DeepReadonly,
+  reactive,
+  readonly,
+  Ref,
+  toRef,
+  UnwrapNestedRefs,
+} from 'vue'
 
-export interface ModuleInterface<
-  S extends State,
-  G extends Getters<S>,
-  M extends Mutations<S>,
+export interface IModule<
+  S extends IState,
+  G extends IGetters,
+  M extends IMutations,
+  SM extends ISubModules,
 > {
-  state: () => S
-  getters: G
-  mutations: M
+  readonly options: IModuleOptions<S, G, M, SM>
+  flatten(): IFlattenedModule<S, G, M, SM>
 }
 
 /* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-empty-interface */
-export interface State extends Record<string, any> {}
+export interface ISubModules extends Record<string, IModule<any, any, any, any>> {}
 
-export interface Getters<S extends State>
-  extends Record<string, (state: DeepReadonly<S>) => any> {}
+export interface IState extends Record<string, any> {}
 
-export type ComputedGetters<S extends State, G extends Getters<S>> = {
-  [key in keyof G]: ComputedRef<ReturnType<G[key]>>
-}
+export interface IGetters extends Record<string, () => any> {}
 
-export interface Mutations<S extends State>
-  extends Record<string, (state: S) => (...args: Array<any>) => void> {}
-
+export interface IMutations extends Record<string, (...args: any) => void> {}
 /* eslint-enable */
 
-export type MappedMutations<S extends State, M extends Mutations<S>> = {
-  [key in keyof M]: ReturnType<M[key]>
+export interface IModuleOptions<
+  S extends IState,
+  G extends IGetters,
+  M extends IMutations,
+  SM extends ISubModules,
+> {
+  name: string
+  version: number
+  stateInit?: () => S
+  getters?: (state: S) => G
+  mutations?: (state: S) => M
+  subModules?: SM
 }
 
-export class Module<S extends State, G extends Getters<S>, M extends Mutations<S>>
-  implements ModuleInterface<S, G, M>
+export class Module<
+  S extends IState,
+  G extends IGetters,
+  M extends IMutations,
+  SM extends ISubModules,
+> implements IModule<S, G, M, SM>
 {
-  state: () => S
-  getters: G
-  mutations: M
+  constructor(public readonly options: IModuleOptions<S, G, M, SM>) {}
 
-  constructor({ state, getters, mutations }: ModuleOptions<S, G, M>) {
-    this.state = state
-    this.getters = getters ?? ({} as G)
-    this.mutations = mutations ?? ({} as M)
+  flatten(): IFlattenedModule<S, G, M, SM> {
+    const flattenedModule = {} as IFlattenedModule<S, G, M, SM>
+
+    // metadata
+    flattenedModule.__metadata = this.getMetadata()
+
+    // state
+    const state = reactive(this.options.stateInit?.() ?? ({} as S))
+    for (const key in state) {
+      // TODO: find the correct typing for this
+      flattenedModule[key] = readonly(toRef(state, key)) as IFlattenedModule<
+        S,
+        G,
+        M,
+        SM
+      >[Extract<keyof UnwrapNestedRefs<S>, string>]
+    }
+
+    // getters
+    const getters = this.options.getters?.(state) ?? ({} as G)
+    for (const key in getters) {
+      // TODO: find the correct typing for this
+      flattenedModule[key] = computed(getters[key]) as IFlattenedModule<
+        S,
+        G,
+        M,
+        SM
+      >[Extract<keyof G, string>]
+    }
+
+    // mutations
+    const mutations = this.options.mutations?.(state) ?? ({} as M)
+    for (const key in mutations) {
+      // TODO: find the correct typing for this and remove any
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      flattenedModule[key] = mutations[key] as any
+    }
+
+    // subModules
+    const subModules = this.options.subModules ?? ({} as SM)
+    for (const key in subModules) {
+      flattenedModule[key] = subModules[key].flatten()
+    }
+
+    return flattenedModule
+  }
+
+  private getMetadata(): IFlattenedModuleMetadata {
+    const { name, version } = this.options
+    return {
+      name,
+      version,
+    }
   }
 }
 
-interface ModuleOptions<S extends State, G extends Getters<S>, M extends Mutations<S>> {
-  state: () => S
-  getters?: G
-  mutations?: M
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type IFlattenedModule<
+  S extends IState,
+  G extends IGetters,
+  M extends IMutations,
+  SM extends ISubModules,
+> = {
+  __metadata: IFlattenedModuleMetadata
+} & {
+  [key in keyof S]: DeepReadonly<Ref<S[key]>>
+} & {
+  [key in keyof G]: ComputedRef<ReturnType<G[key]>>
+} & M & {
+    [key in keyof NonNullable<SM>]: IFlattenedModule<
+      ReturnType<NonNullable<NonNullable<SM>[key]['options']['stateInit']>>,
+      ReturnType<NonNullable<NonNullable<SM>[key]['options']['getters']>>,
+      ReturnType<NonNullable<NonNullable<SM>[key]['options']['mutations']>>,
+      ReturnType<NonNullable<NonNullable<SM>[key]['options']['subModules']>>
+    >
+  }
+
+export interface ModuleDataForLocalStorage<S extends IState> {
+  version: number
+  state: S
 }
 
-export type FlattenedModule<
-  S extends State,
-  G extends Getters<S>,
-  M extends Mutations<S>,
-> = ReadonlyStateRefs<S> & ComputedGetters<S, G> & MappedMutations<S, M>
-
-export type ReadonlyStateRefs<S extends State> = {
-  [key in keyof S]: DeepReadonly<Ref<S[key]>>
+export interface IFlattenedModuleMetadata {
+  name: string
+  version: number
 }

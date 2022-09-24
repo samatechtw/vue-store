107a453d0340345fc920eaab7a7cdf5a3d2eef9a
Sam Pullman <sampullman@gmail.com>
2022-09-24 19:44:11 +0800

    ci: update commitlint rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const typeEnum = require('@commitlint/config-angular-type-enum')
+
 module.exports = {
   extends: ['@commitlint/config-angular'],
+  rules: {
+    'type-enum': [2, 'always', [...typeEnum.value(), 'release']],
+  },
 }

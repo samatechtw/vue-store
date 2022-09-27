08c2d5a98393ddf2010f7f920fdd216400f92a98
abemscac <abe.msc.ac@gmail.com>
2022-09-27 17:34:17 +0800

    feat: local storage plugin clear on new version

--- lib/plugins.ts
+++ lib/plugins.ts
@@ -1,5 +1,10 @@
 import { IGenericModule, IPlugin, IState } from './interfaces'
 
+interface ILocalStoragePluginState<S extends IState> {
+  state: S
+  __version: number
+}
+
 export const LocalStoragePlugin = <S extends IState>(
   module: IGenericModule<S>,
 ): IPlugin<S> => {
@@ -9,10 +14,22 @@ export const LocalStoragePlugin = <S extends IState>(
       if (!stateString) {
         return state
       }
-      return JSON.parse(stateString)
+      const payload = JSON.parse(stateString) as ILocalStoragePluginState<S>
+      if (payload.__version !== module.options.version) {
+        console.warn(
+          // eslint-disable-next-line max-len
+          `Module with name '${module.options.name}' changed from ${payload.__version} to ${module.options.version}. The state has been reset.`,
+        )
+        return state
+      }
+      return payload.state
     },
     onDataChange: (value: S) => {
-      localStorage.setItem(module.options.name, JSON.stringify(value))
+      const payload: ILocalStoragePluginState<S> = {
+        state: value,
+        __version: module.options.version,
+      }
+      localStorage.setItem(module.options.name, JSON.stringify(payload))
     },
   }
 }
